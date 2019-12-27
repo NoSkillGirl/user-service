bf8a6475f7cc183600863dd7316c9b6d83dd0016
NoSkillGirl <singhpooja240393@gmail.com>
2019-12-27 19:36:16 +0530

    changes in User Model

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	// "github.com/NoSkillGirl/user-service/models"
 	"github.com/NoSkillGirl/user-service/routers"
 )
 
 func main() {
 	// UserRoutes Initilization
+
+	// e, u := models.AddUser("Blah", "8908", "fkjs@gmail.com", "fdskjfks")
+	// fmt.Println(e, u)
 	routers.UserRoutes()
 	http.ListenAndServe(":8082", nil)
 }
